189d213af163078ec614b111eb7dbc1b84a8a5da
Segev <segev.kest@gmail.com>
2022-05-17 22:21:41 +0300

    Created Term Class - contructors, getters and setter

---
+++ src/TermWithMeaning.java
@@ -0,0 +1,35 @@
+
+// The class will represent the single Term with its meanings in the dictionary
+// the dictionary will include Terms With their meanings
+
+public class TermWithMeaning {
+
+	private String termName;
+	private String meaning;
+	
+	
+	public TermWithMeaning(String newTerm, String newMeaning) {
+		
+		termName = newTerm;
+		meaning = newMeaning;
+		
+	}
+	
+	public String getTermName() {
+		return termName;
+	}
+	
+	
+	public String getMeaning() {
+		return meaning;
+	}
+	
+	
+	public void setMeaning(String newMeaning) {
+		
+		if (newMeaning != null)		
+			meaning = newMeaning;
+		
+		
+	}
+}
