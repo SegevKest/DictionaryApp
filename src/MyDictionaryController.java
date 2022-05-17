f53be54156057431eff8ed446fedf9a66a035f54
Segev <segev.kest@gmail.com>
2022-05-17 08:42:22 +0300

    First commit - init screen and files

---
+++ src/MyDictionaryController.java
@@ -0,0 +1,35 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class MyDictionaryController {
+
+    @FXML
+    private ListView<?> dictionaryList;
+
+    @FXML
+    private Button loadDicFromFileBtn;
+
+    @FXML
+    private Button saveToFileBtn;
+
+    
+    
+    
+    public void initialize() {
+		
+    }
+
+    
+    @FXML
+    void loadFromFile(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveDicToFile(MouseEvent event) {
+
+    }
+
+}
