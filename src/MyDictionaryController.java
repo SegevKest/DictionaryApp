7b9c2f2ade41c316f2c2acc72c31e8fe87ed6e64
Segev <segev.kest@gmail.com>
2022-05-18 08:47:51 +0300

    Created Layout of app - all forms

--- src/MyDictionaryController.java
+++ src/MyDictionaryController.java
@@ -1,35 +1,54 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class MyDictionaryController {
 
-    @FXML
-    private ListView<?> dictionaryList;
-
-    @FXML
-    private Button loadDicFromFileBtn;
-
-    @FXML
-    private Button saveToFileBtn;
-
+    @FXML private Button deleteTermBtn;
+    @FXML private TextField deleteTermInput;
+    @FXML private ListView<?> dictionaryList;
+    @FXML private Button insertNewTermBtn;
+    @FXML private TextArea insertNewTermMeaningInput;
+    @FXML private TextField insertNewTermNameInput;
+    @FXML private Button loadDicFromFileBtn;
+    @FXML private Button saveToFileBtn;
+    @FXML private Button searchBtn;
+    @FXML private TextField searchTermInput;
+    @FXML private Button updateMeaningBtn;
+    @FXML private TextArea updateNewTermMeaning;
+    @FXML private TextField updateTermNameInput;
     
     
     
-    public void initialize() {
-		
+
+
+
+    @FXML void onDeleteTerm(MouseEvent event) {
+    	System.out.println("Delete");
+    } 
+
+    @FXML void onInsertNewTerm(MouseEvent event) {
+    	System.out.println("Insert");
+    }
+
+    @FXML void onSearchTerm(MouseEvent event) {
+    	System.out.println("Search");
+    }
+ 
+    @FXML void onUpdateMeaning(MouseEvent event) {
+    	System.out.println("Update");
     }
 
     
-    @FXML
-    void loadFromFile(MouseEvent event) {
-
+    // File handling
+    @FXML void loadFromFile(MouseEvent event) {
+		System.out.println("Load");
     }
-
-    @FXML
-    void saveDicToFile(MouseEvent event) {
-
+    @FXML void saveDicToFile(MouseEvent event) {
+    	System.out.println("save");
     }
 
 }
